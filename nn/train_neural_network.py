bf1d3cb0b04697b45e8db91ee8949b4a3d7bec68
antantantant <renyi.tsinghua@gmail.com>
2018-02-20 18:46:10 -0700

    testing nn

--- nn/train_neural_network.py
+++ nn/train_neural_network.py
@@ -4,33 +4,60 @@ from keras.layers import Dense, Activation, Dropout
 from keras.callbacks import EarlyStopping
 import csv
 
+#################################################
+# try to scale input data
+s_other_y          = [0, 1]
+s_self_x           = [-2, 2]
+s_self_y           = [0, 1]
+s_desired_other_x  = [-2, 2]
+s_desired_other_y  = [0, 1]
+c_other            = [20, 100]
+#################################################
+
 # Read file
-with open('train_data_50000.txt') as f:
+with open('train_data_10000.txt') as f:
     reader = csv.reader(f, delimiter="\t")
     d = array(list(reader))
 
 
 # Input initialization
 inp_dim = 6
-out_dim = 2
+out_dim = 40
 
-data_size = 50000
-validation_ind = 40000
-test_ind = 45000
+data_size = 10000
+validation_ind = 8000
+test_ind = 9000
 
 # Import the data
-train_x = array(d[0:validation_ind, 0:inp_dim])
+train_x = d[0:validation_ind, 0:inp_dim].astype('float32')
+train_x[:,0] = (train_x[:,0] - s_other_y[0])/(s_other_y[1]-s_other_y[0])
+train_x[:,1] = (train_x[:,1] - s_self_x[0])/(s_self_x[1]-s_self_x[0])
+train_x[:,2] = (train_x[:,2] - s_self_y[0])/(s_self_y[1]-s_self_y[0])
+train_x[:,3] = (train_x[:,3] - s_desired_other_x[0])/(s_desired_other_x[1]-s_desired_other_x[0])
+train_x[:,4] = (train_x[:,4] - s_desired_other_y[0])/(s_desired_other_y[1]-s_desired_other_y[0])
+train_x[:,5] = (train_x[:,5] - c_other[0])/(c_other[1]-c_other[0])
+train_x = array(train_x)
 train_y = array(d[0:validation_ind, inp_dim:inp_dim+out_dim])
 
-val_x = array(d[validation_ind:test_ind, 0:inp_dim])
+val_x = d[validation_ind:test_ind, 0:inp_dim].astype('float32')
+val_x[:,0] = (val_x[:,0] - s_other_y[0])/(s_other_y[1]-s_other_y[0])
+val_x[:,1] = (val_x[:,1] - s_self_x[0])/(s_self_x[1]-s_self_x[0])
+val_x[:,2] = (val_x[:,2] - s_self_y[0])/(s_self_y[1]-s_self_y[0])
+val_x[:,3] = (val_x[:,3] - s_desired_other_x[0])/(s_desired_other_x[1]-s_desired_other_x[0])
+val_x[:,4] = (val_x[:,4] - s_desired_other_y[0])/(s_desired_other_y[1]-s_desired_other_y[0])
+val_x[:,5] = (val_x[:,5] - c_other[0])/(c_other[1]-c_other[0])
+val_x = array(val_x)
 val_y = array(d[validation_ind:test_ind, inp_dim:inp_dim+out_dim])
 
-test_x = array(d[test_ind:data_size, 0:inp_dim])
+test_x = d[test_ind:data_size, 0:inp_dim].astype('float32')
+test_x[:,0] = (test_x[:,0] - s_other_y[0])/(s_other_y[1]-s_other_y[0])
+test_x[:,1] = (test_x[:,1] - s_self_x[0])/(s_self_x[1]-s_self_x[0])
+test_x[:,2] = (test_x[:,2] - s_self_y[0])/(s_self_y[1]-s_self_y[0])
+test_x[:,3] = (test_x[:,3] - s_desired_other_x[0])/(s_desired_other_x[1]-s_desired_other_x[0])
+test_x[:,4] = (test_x[:,4] - s_desired_other_y[0])/(s_desired_other_y[1]-s_desired_other_y[0])
+test_x[:,5] = (test_x[:,5] - c_other[0])/(c_other[1]-c_other[0])
+test_x = array(test_x)
 test_y = array(d[test_ind:data_size, inp_dim:inp_dim+out_dim])
-
-train_x = train_x.astype('float32')
-val_x   = val_x.astype('float32')
-test_x  = test_x.astype('float32')
 
 train_y = train_y.astype('float32')
 val_y   = val_y.astype('float32')
@@ -39,10 +66,13 @@ test_y  = test_y.astype('float32')
 # Network Model
 model = Sequential()
 
-model.add(Dense(50, input_shape=(inp_dim,)))
+model.add(Dense(16, input_shape=(inp_dim,)))
 model.add(Activation('relu'))
 model.add(Dropout(0.2))  # it's optional
-model.add(Dense(10))
+model.add(Dense(32))
+model.add(Activation('relu'))
+model.add(Dropout(0.2))  # it's optional
+model.add(Dense(64))
 model.add(Activation('relu'))
 model.add(Dropout(0.2))  # it's optional
 model.add(Dense(out_dim))
@@ -52,9 +82,9 @@ model.add(Activation('sigmoid'))  # you may change the activation here based on 
 model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
 model.summary()
 
-early_stop = EarlyStopping(monitor='val_loss', min_delta=0, patience=0, verbose=1, mode='min')
-model.fit(train_x, train_y, batch_size=validation_ind, epochs=100, verbose=1, validation_data=(val_x, val_y),
-          callbacks=[early_stop], initial_epoch=10)  # please read the tutorial for the guide of parameter tuning
+# early_stop = EarlyStopping(monitor='val_loss', min_delta=0, patience=0, verbose=1, mode='min')
+model.fit(train_x, train_y, batch_size=validation_ind, epochs=5000, verbose=1, validation_data=(val_x, val_y),
+          callbacks=[], initial_epoch=10)  # please read the tutorial for the guide of parameter tuning
 
 model.save('action_prediction_model.h5')  # save the model
 
