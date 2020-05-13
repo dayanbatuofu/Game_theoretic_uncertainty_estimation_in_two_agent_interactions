c52d2cc4461207dda2c1a2955b794234798b4930
ychen837 <yichenn37@gmail.com>
2020-05-13 12:33:27 -0700

    mdp skelecton for inference model

--- inference_model.py
+++ inference_model.py
@@ -1,4 +1,5 @@
 import numpy as np
+#from sklearn.processing import normalize
 # TODO pytorch version
 
 
@@ -27,11 +28,58 @@ class InferenceModel:
         system state (agent.state[-1] for all agent), and prior dist. of par
         :return:
         """
-
+        def q_values():
+            """
+            refer to classic.py, car.py
+            Calculates hardmax Q-value given state-action pair.
+            Q(s,a) = R(s,a)  +V(s')
+            """
+            pass
+        def transition_probabilities():
+            """
+            Refer to mdp.py
+            :return:
+            """
+            pass
+        def action_probabilities():
+            """
+            refer to mdp.py
+            calculates probability distribution of action given hardmax Q values
+            Uses:
+            1. Softmax algorithm
+            2. Q-value given state action pair (s, a)
+            3. beta: "rationality coefficient"
+            => P(uH|xH;beta,theta) = exp(beta*QH(xH,uH;theta))/sum_u_tilde[exp(beta*QH(xH,u_tilde;theta))]
+            :return:
+            """
+            pass
+        def traj_probabilities():
+            """
+            refer to mdp.py
+            multiply over action probabilities to obtain trajectory probabilities given (s, a)
+            :return:
+            """
+            pass
+        def value_iter():
+            """
+            refer to hardmax.py
+            Calculate V where ith entry is the value of starting at state s till i
+            :return:
+            """
+            pass
         pass
-
+        def transition_helper(g, s, a, alert_illegal = False):
+            """
+            refer to classic.py, car.py
+            defines the transition when action a is taken in state s
+            :return:
+            """
     @staticmethod
     def empathetic_inference():
+        """
+        When QH also depends on xM,uM
+        :return:
+        """
         # TODO: implement proposed
 
         # predicted_intent_other, predicted_intent_self, predicted_policy_other, predicted_policy_self
